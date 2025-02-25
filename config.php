6325c037bb83057e04bf91ab53bb4a0ab0368775
Carlo Baltazar <gnabolla@gmail.com>
2025-02-26 00:04:12 +0800

    for prod

--- config.php
+++ config.php
@@ -5,9 +5,11 @@ return [
         'host' => 'localhost',
         'port' => 3306,
         'dbname' => 'billing_db',
-        'charset' => 'utf8mb4'
+        'charset' => 'utf8mb4',
+        'username' => 'root',
+        'password' => 'StrongPassword123!',
     ],
     'app' => [
-        'base_path' => '/billing'
+        'base_path' => 'http://174.138.24.254/billing'
     ]
 ];
